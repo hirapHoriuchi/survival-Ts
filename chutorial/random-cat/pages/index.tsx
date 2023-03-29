a1135ca3f99c10084af273adfff5484d3bcb3946
平川優太 <hirakawayuuta@hirakawayuutanoMacBook-Air.local>
2023-03-29 15:59:28 +0900

    second commit

--- chutorial/random-cat/pages/index.tsx
+++ chutorial/random-cat/pages/index.tsx
@@ -12,20 +12,21 @@
 
 // export default IndexPage
 
-import { NextPage } from "next";
+import { GetServerSideProps,NextPage } from "next";
 import { useEffect, useState } from "react";
+import styles from "./index.module.css";
 
-const IndexPage: NextPage = () => {
+const IndexPage: NextPage<Props> = ({initialImageUrl}) => {
   // ❶ useStateを使って状態を定義する
-  const [imageUrl, setImageUrl] = useState("");
-  const [loading, setLoading] = useState(true);
+  const [imageUrl, setImageUrl] = useState(initialImageUrl);
+  const [loading, setLoading] = useState(false);
   // ❷ マウント時に画像を読み込む宣言
-  useEffect(() => {
-    fetchImage().then((newImage) => {
-      setImageUrl(newImage.url); // 画像URLの状態を更新する
-      setLoading(false); // ローディング状態を更新する
-    });
-  }, []);
+  // useEffect(() => {
+  //   fetchImage().then((newImage) => {
+  //     setImageUrl(newImage.url); // 画像URLの状態を更新する
+  //     setLoading(false); // ローディング状態を更新する
+  //   });
+  // }, []);
   // ボタンをクリックしたときに画像を読み込む処理
   const handleClick = async () => {
     setLoading(true); // 読込中フラグを立てる
@@ -34,19 +35,36 @@ const IndexPage: NextPage = () => {
     setLoading(false); // 読込中フラグを倒す
   };
   return (
-    <div>
-      <button onClick={handleClick}>他のにゃんこも見る</button>
-      <div>{loading || <img src={imageUrl} />}</div>
+    <div className={styles.page}>
+      <button onClick={handleClick} className={styles.button}>他のにゃんこも見る</button>
+      <div className={styles.frame}>
+        {loading || <img src={imageUrl} className={styles.img} />}</div>
     </div>
   );
 };
 export default IndexPage;
 
+// getServerSidePropsから渡されるpropsの型
+type Props = {
+  initialImageUrl: string;
+};
+
+// サーバーサイドで実行する処理
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const image = await fetchImage();
+  return {
+    props: {
+      initialImageUrl: image.url,
+    },
+  };
+};
+
+
 type Image = {
   url: string;
 };
 
-const fetchImage = async () => {
+const fetchImage = async (): Promise<Image> => {
   const res = await fetch("https://api.thecatapi.com/v1/images/search");
   const images = await res.json();
   console.log(images);
